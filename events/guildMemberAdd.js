7f8cfeb778e76ca60de73dd96ae05b9b3697e97c
Oogie Boogie <diego.89@gmail.com>
2018-09-15 22:00:00 -0300

    New members are now assigned default roles

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,12 +5,21 @@ module.exports = (client, member) => {
   const settings = client.getGuildSettings(member.guild);
 
   // If welcome is off, don't proceed (don't welcome the user)
-  if (settings.welcomeEnabled !== "true") return;
+	if (settings.welcomeEnabled === 'true') {
+		// Replace the placeholders in the welcome message with actual data
+		const welcomeMessage = settings.welcomeMessage.replace('{{user}}', member.user.tag);
 
-  // Replace the placeholders in the welcome message with actual data
-  const welcomeMessage = settings.welcomeMessage.replace("{{user}}", member.user.tag);
+		// Send the welcome message to the welcome channel
+		// There's a place for more configs here.
+		member.guild.channels.find('name', settings.welcomeChannel).send(welcomeMessage).catch(console.error);
+	}
 
-  // Send the welcome message to the welcome channel
-  // There's a place for more configs here.
-  member.guild.channels.find(c => c.name === settings.welcomeChannel).send(welcomeMessage).catch(console.error);
+	// Check if auto assinging roles is enabled
+	if (settings.newUserRoleEnabled === 'true') {
+		client.logger.log(`[MEMBER JOINED] ${member.user.tag}`);
+		// Get the default role
+		var role = member.guild.roles.find('name', settings.newUserRole);
+		// Assing said role
+		member.addRole(role).catch(console.error);
+	}
 };
